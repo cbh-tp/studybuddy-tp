ff48f4e9993ed0de505ab0b07402fe9b6296f885
cbh-tp <cbong8702@gmail.com>
2026-01-15 02:22:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Home from './pages/Home';
 import MyBookings from './pages/MyBookings';
 import TutorProfile from './pages/TutorProfile';
